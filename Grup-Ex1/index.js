bb1ac295dc636f843422d178f7e027f91e280c34
nataliakatutu <nataliakatutu26@gmail.com>
2023-02-15 16:19:47 +0800

    menambahkan grup exercise 1

---
+++ Grup-Ex1/index.js
@@ -0,0 +1,116 @@
+// Group Exercise By Tokyo
+//
+// Katutu, Natalia 
+// Kalalo, Sheren 
+// Sundah, Mia
+// Luntungan, Hendra Ucok
+// Kaminang, Anjelita
+// Patuli, Cherlita
+
+//Array Exercises
+//    let people = ["Greg", "Mary", "Devon", "James"];
+    
+    // nomor 1
+    
+    // for(let i =0; i< people.length; i++){
+    // console.log(people[i]);
+    // }
+    // console.log("~~~~~~~~");
+
+    // nomor 2
+
+    // people.forEach()
+    // console.log(people[i])
+    // people.forEach((Element) => console.log(Element))
+    // console.log("~~~~~~~~");
+
+    // nomor 3
+
+    //people.shift()
+    // console.log(people)
+
+    // nomor 4 
+    
+    //people.pop()
+    // console.log(people)
+
+    // nomor 5
+
+    //people.unshift("Matt")
+    // console.log(people)
+
+    // nomor 6
+    
+    //people.push("Tokyo")
+    // console.log(people)
+
+    // nomor 7
+    
+    // for(let i =0; i< people.length; i++){
+    //     if(i > 1){
+    //         break;
+    //     }
+    //     console.log(people[i]);
+    // }
+    // console.log("~~~~~~~~~");
+
+    // nomor 8 
+
+    // people.slice(1);
+    // console.log(people.slice(1))
+    // console.log("~~~~~~~~~");
+
+    // nomor 9
+
+    // people = ["Matt", "Mary", "Devon", "Tokyo"];
+    // people.splice(2,1,"Elizabeth", "Artie");
+    // console.log(people)
+    // console.log("~~~~~~~~~");
+
+    // nomor 10
+
+    // let withBob = people.concat("Bob")
+    // console.log(withBob)
+
+
+
+//Object Exercise
+
+// let programming = {
+//     languages: ["JavaScript", "Python", "Ruby"],
+//     isChallenging: true,
+//     isRewarding: true,
+//     difficulty: 8,
+//     jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+// }
+
+//nomor 1
+
+// programming.languages.push("Go");
+// console.log(programming.languages.push)
+
+//nomor 2
+
+// programming.difficulty = 7;
+
+//nomor 3
+
+// delete programming.jokes;
+
+//nomor 4
+
+// programming.isFun = true;
+// console.log(programming)
+
+//nomor 5
+
+//  const names =["Javascript", "Python"];
+//  const newProgramming = names.map((name, index) => {
+//      return index.toString() + '. ' +name;
+//  });
+//  console.log(newProgramming);
+
+// let newProgramming = programming.languages.map(function(item,index){
+//     return index+" "+item;
+// });
+// console.log(newProgramming);
